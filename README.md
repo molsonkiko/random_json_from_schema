aa4ece5a9cc9d4fadfc38ccf2054002d8e742ef2
molsonkiko <46202915+molsonkiko@users.noreply.github.com>
2022-12-07 17:28:52 -0800

    fix import bug, add auto-generation of schema

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # random_json_from_schema
 
-CLI app and function (random_thing) for generating random JSON from a [JSON schema](https://json-schema.org/). Supports the following keywords:
+CLI app and function (random_thing) for generating random JSON from a [JSON schema](https://json-schema.org/). 
+
+If [GenSON](https://github.com/wolverdude/GenSON) is installed, you can optionally generate random JSON that matches the schema of *any* JSON file, rather than having to make a JSON schema first.
+
+Supports the following keywords:
+
 ## all JSON
 * [anyOf](https://json-schema.org/draft/2020-12/json-schema-core.html#name-anyof)
 * type
--- __main__.py
+++ __main__.py
@@ -1,15 +1,29 @@
+import json
+import sys
 from .json_from_schema import random_thing
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('fname', help='name of schema file')
+    parser.add_argument('-i', type=int, default=0, help='number of spaces to indent if pretty-printing the JSON. If 0, print compressed')
+    parser.add_argument('--genSchema', action='store_true', help='generate a JSON schema from a non-schema JSON file?')
     parser.add_argument('--minItems', type=int, default=0, help='min number of items in an array')
     parser.add_argument('--maxItems', type=int, default=10, help='max number of items in an array')
-    parser.add_argument('-i', type=int, default=0, help='number of spaces to indent if pretty-printing the JSON. If 0, print compressed')
     args = parser.parse_args()
     with open(args.fname) as f:
         schema = json.load(f)
+    schema_generated = False
+    if args.genSchema:
+        try:
+            import genson
+        except ImportError:
+            print('Try installing GenSON ("python -m pip install genson") before using the --genSchema option')
+            sys.exit(1)
+        json_ = schema
+        builder = genson.SchemaBuilder()
+        builder.add_object(json_)
+        schema = builder.to_schema()
     result = random_thing(schema, args.minItems, args.maxItems)
     if args.i == 0:
         print(json.dumps(result))
